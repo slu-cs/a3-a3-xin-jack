ba8dcc4bfb85673151e60b23058a4873118db6bc
jepatt16 <jepatt16@gmail.com>
2020-02-27 14:05:55 -0500

    add rows to table

--- index.js
+++ index.js
@@ -6,15 +6,20 @@ $('form').on('submit', function(event) {
   event.preventDefault(); // Don't really submit the form
 
   const input = $('input'); // Turn input into a JQuery object
-  const li = $(`<li>${input.val()}</li>`); // create li w/ text
+  const li = $(`<li class='item${counter}'>${input.val()}</li>`); // create li w/ text
   li.append($(`<button type='submit'>-</button>`));
   li.append($(`<button type='submit'>+</button>`));
   li.append($(`<button type='submit'>Remove</button>`)); // add buttons
 
-  input.val(''); // reset text
-
-  counter++; // increment id counter
+  const tr = $(`<tr class='item${counter}'>
+                  <td>${input.val()}</td>
+                  <td>0</td>
+                </tr>`);
 
   $('ul').append(li); // Add to the page
+  $('table').append(tr);
+
+  input.val(''); // reset text
+  counter++; // increment id counter
 
 });
