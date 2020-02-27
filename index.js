98bb05ce717139a187620fdddb6d6200ee704b43
jepatt16 <jepatt16@gmail.com>
2020-02-27 13:54:38 -0500

    cleaned up code to add items to list

--- index.js
+++ index.js
@@ -1,27 +1,20 @@
-let counter = 0;
+let counter = 0; // counter for identifying options
 
 // Respond to submit events
 $('form').on('submit', function(event) {
   const form = $(this); // JQuery object representing the form
   event.preventDefault(); // Don't really submit the form
 
-  // Create a new list
-  const list = $('<ul></ul>');
+  const input = $('input'); // Turn input into a JQuery object
+  const li = $(`<li>${input.val()}</li>`); // create li w/ text
+  li.append($(`<button type='submit'>-</button>`));
+  li.append($(`<button type='submit'>+</button>`));
+  li.append($(`<button type='submit'>Remove</button>`)); // add buttons
 
-  // Translate each input value into a list item
-  const inputs = form.find('input'); // JQuery object represent all inputs
-  for (let input of inputs) {
-    input = $(input); // Turn into a JQuery object
-    const li = $(`<li>${input.val()}<button type='submit'>-</button>  <button type='submit'>+</button>  <button type='submit'>Remove</button></li> `); // New list item
-    list.append(li);
-    input.val('');
-  }
+  input.val(''); // reset text
 
-  counter++;
-  const p = $(`<p>Options </p>`);
+  counter++; // increment id counter
 
-  // Add to the page
-
-  $('#voting').append(list);
+  $('ul').append(li); // Add to the page
 
 });
