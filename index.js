ab73ee6f7ff768396b35fed57da81e3c5f9089b8
Xin Tao <xtao17@stlawu.edu>
2020-03-01 15:05:14 -0500

    remove added

--- index.js
+++ index.js
@@ -2,19 +2,30 @@ let counter = 0; // counter for identifying options
 
 // Respond to submit events
 $('form').on('submit', function(event) {
+
   const form = $(this); // JQuery object representing the form
   event.preventDefault(); // Don't really submit the form
 
   const input = $('input'); // Turn input into a JQuery object
-  const li = $(`<li class='item${counter}'>${input.val()}</li>`); // create li w/ text
-  li.append($(`<button type='submit'>-</button>`));
-  li.append($(`<button type='submit'>+</button>`));
-  li.append($(`<button type='submit'>Remove</button>`)); // add buttons
 
+  const li = $(`<li class='item${counter}'>${input.val()}</li>`); // create li w/ text
+  li.append($(`<button type='submit' id="-">-</button>`));
+  li.append($(`<button type='submit' id="+">+</button>`));
+  li.append($(`<button type='submit' id="remove">Remove</button>`)); // add buttons
+  count=0
   const tr = $(`<tr class='item${counter}'>
                   <td>${input.val()}</td>
-                  <td>0</td>
+                  <td>${count}</td>
                 </tr>`); // add row to table
+
+
+  $('ul').on('click', '#remove', function(event) {
+         $(this).closest('li').remove();
+         $('table').closest('tr').remove();
+       });
+
+
+
 
   $('ul').append(li); // Add to the page
   $('table').append(tr);
