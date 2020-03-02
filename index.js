ad9c3fac60c84fadd2bae77324394b7bba9d234c
jepatt16 <jepatt16@gmail.com>
2020-03-02 15:11:25 -0500

    check if input is blank

--- index.js
+++ index.js
@@ -7,22 +7,22 @@ $('form').on('submit', function(event) {
   event.preventDefault(); // Don't really submit the form
 
   const input = $('input'); // Turn input into a JQuery object
+  if (input.val() !== ''){ // check if input is blank
+    const li = $(`<li class='item${counter}'>${input.val()}</li>`); // create li w/ text
+    li.append($(`<button id="minus">-</button>`));
+    li.append($(`<button id="plus">+</button>`));
+    li.append($(`<button id="remove">Remove</button>`)); // add buttons
+    const tr = $(`<tr class='item${counter}'>
+                    <td>${input.val()}</td>
+                    <td>0</td>
+                  </tr>`); // add row to table
 
-  const li = $(`<li class='item${counter}'>${input.val()}</li>`); // create li w/ text
-  li.append($(`<button id="minus">-</button>`));
-  li.append($(`<button id="plus">+</button>`));
-  li.append($(`<button id="remove">Remove</button>`)); // add buttons
-  const tr = $(`<tr class='item${counter}'>
-                  <td>${input.val()}</td>
-                  <td>0</td>
-                </tr>`); // add row to table
+    $('ul').append(li); // Add to the page
+    $('table').append(tr);
 
-  $('ul').append(li); // Add to the page
-  $('table').append(tr);
-
-  input.val(''); // reset text
-  counter++; // increment id counter
-
+    input.val(''); // reset text
+    counter++; // increment id counter
+  };
 });
 
 $('ul').on('click', '#remove', function(event) {
