6828ddd63a60c9c161e5aa0a5a707a7553794b21
jepatt16 <jepatt16@gmail.com>
2020-03-02 14:18:39 -0500

    got rid of submit tags on other buttons, commenting

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ $('form').on('submit', function(event) {
   const input = $('input'); // Turn input into a JQuery object
 
   const li = $(`<li class='item${counter}'>${input.val()}</li>`); // create li w/ text
-  li.append($(`<button type='submit' id="minus">-</button>`));
-  li.append($(`<button type='submit' id="plus">+</button>`));
-  li.append($(`<button type='submit' id="remove">Remove</button>`)); // add buttons
+  li.append($(`<button id="minus">-</button>`));
+  li.append($(`<button id="plus">+</button>`));
+  li.append($(`<button id="remove">Remove</button>`)); // add buttons
   const tr = $(`<tr class='item${counter}'>
                   <td>${input.val()}</td>
                   <td>0</td>
@@ -20,9 +20,9 @@ $('form').on('submit', function(event) {
 
   $('ul').on('click', '#remove', function(event) {
          event.preventDefault();
-         const cl = $(this).closest('li').attr('class');
-         $(this).closest('li').remove();
-         $('table').find(`.${cl}`).remove();
+         const cl = $(this).closest('li').attr('class'); // get item number
+         $(this).closest('li').remove(); // remove item from list
+         $('table').find(`.${cl}`).remove(); // remove row from table
        });
 
   $('ul').on('click', '#plus', function(event){
@@ -31,6 +31,7 @@ $('form').on('submit', function(event) {
     const cl = li.attr('class'); // get item number
     const tr = $('table').find(`.${cl}`); // find corresponding row
     const val = parseInt(tr.find('td')[1].textContent); // get vote number from table
+
   });
 
 
