6c2c98c54f398208e52f043017444729246ab62d
jepatt16 <jepatt16@gmail.com>
2020-03-02 14:14:47 -0500

    gets text value from table

--- index.js
+++ index.js
@@ -9,24 +9,29 @@ $('form').on('submit', function(event) {
   const input = $('input'); // Turn input into a JQuery object
 
   const li = $(`<li class='item${counter}'>${input.val()}</li>`); // create li w/ text
-  li.append($(`<button type='submit' id="-">-</button>`));
-  li.append($(`<button type='submit' id="+">+</button>`));
+  li.append($(`<button type='submit' id="minus">-</button>`));
+  li.append($(`<button type='submit' id="plus">+</button>`));
   li.append($(`<button type='submit' id="remove">Remove</button>`)); // add buttons
-  count=0
   const tr = $(`<tr class='item${counter}'>
                   <td>${input.val()}</td>
-                  <td>${count}</td>
+                  <td>0</td>
                 </tr>`); // add row to table
 
 
   $('ul').on('click', '#remove', function(event) {
+         event.preventDefault();
          const cl = $(this).closest('li').attr('class');
-         
          $(this).closest('li').remove();
          $('table').find(`.${cl}`).remove();
        });
 
-
+  $('ul').on('click', '#plus', function(event){
+    event.preventDefault();
+    const li = $(this).closest('li'); // find event listener
+    const cl = li.attr('class'); // get item number
+    const tr = $('table').find(`.${cl}`); // find corresponding row
+    const val = parseInt(tr.find('td')[1].textContent); // get vote number from table
+  });
 
 
   $('ul').append(li); // Add to the page
